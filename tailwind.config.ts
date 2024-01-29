5555fe850de06cffea1cfbffb675389097d272cd
chrischenld <christopherchen0@gmail.com>
2024-01-28 20:16:20 -0800

    Create layout for pages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,49 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		fontSize: {
+			xs: "0.75rem",
+			sm: "0.875rem",
+			base: "1rem",
+			xl: "1.25rem",
+			"2xl": "1.563rem",
+			"3xl": "1.953rem",
+			"4xl": "2.441rem",
+			"5xl": "3.052rem",
+		},
+		extend: {
+			height: {
+				"128": "32rem",
+				"144": "36rem",
+			},
+			fontFamily: {
+				fragmentMono: ["var(--font-fragment-mono)"],
+				pantasia: ["var(--font-pantasia)"],
+			},
+		},
+		screens: {
+			sm: "300px",
+			// => @media (min-width: 640px) { ... }
+
+			md: "600px",
+			// => @media (min-width: 768px) { ... }
+
+			lg: "960px",
+			// => @media (min-width: 1024px) { ... }
+
+			// xl: "1280px",
+			// => @media (min-width: 1280px) { ... }
+
+			// "2xl": "1536px",
+			// => @media (min-width: 1536px) { ... }
+		},
+	},
+	plugins: [],
 };
 export default config;
